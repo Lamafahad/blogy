82ceeb5a24c3a75c87ad1a810a594ce16090cfc9
Lamafahad <lamafa97@gmail.com>
2019-12-05 16:25:55 +0300

    Add delete method

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
-// Require Necessary NPM Packages
+// Require necessary NPM Packages
 const express = require('express');
 const mongoose = require('mongoose');
-
 // Require Route Files
 const indexRouter = require('./app/routes/index');
-const articleRouter = require('./app/routes/articles');
-
+const articlesRouter = require('./app/routes/articles');
 // Require DB Configuration File
 const db = require('./config/db');
-
 // Establish Database Connection
-mongoose.connect(db, {useNewUrlParser: true});
-
-// to make sure its Connected to Mongo
+mongoose.connect(db, { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
-    console.log('Connected to Mongo');
+  console.log('Connected to Mongo');
 })
-
-
-// Instantiate Express Application Object || Make an Express App
+// Instantiate Express Application Object
 const app = express();
-
-// Define a PORT for the API to run on 
-const port = process.env.PORT || 5000; // if the env has a number use it if not use 5000 || proce... comes from node
-
-    /*** ROUTES ***/
+// Define PORT for the API to run on
+const port = process.env.PORT || 5000;
+/*** Middleware ***/
+// Add `bodyParser` middleware which will parse JSON requests into
+// JS objects before they reach the route files.
+//
+// The method `.use` sets up middleware for the Express application
+app.use(express.json());
+/*** Routes ***/
 // Mount imported Routers
 app.use(indexRouter);
-app.use(articleRouter);
-
+app.use(articlesRouter);
 // Start the server to listen for requests on a given port
-app.listen(port, () =>{
-    console.log(`blogy is listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`blogy is listening on port ${port}`);
 });

bc391f903c8c8e1c85b3a34f9a7122181da9b611
Lamafahad <lamafa97@gmail.com>
2019-12-05 14:23:34 +0300

    addedmodel

---
+++ app/routes/articles.js
@@ -0,0 +1,60 @@
+// Require Necessary NPM Packages
+const express = require('express');
+
+// Require Mongoose Model for Article
+const Article = require('../models/article');
+
+// Instantiate a Router (mini app that only handles routes)
+const router = express.Router();
+
+/*
+* Action:       INDEX
+* Method:       GET
+* URI:          /api/articles
+* Description:  Get All Articles
+*/
+
+router.get('/api/articles', (req,res) => {
+    Article.find()
+            // Return all Articles as an Array
+    .then((articles) => {
+        res.status(200).json({message: 'Get All Articles'});
+    })
+    // Catch any errors that might occur
+    .catch((error) => {
+        res.status(500).json({error: error});
+    });
+});
+
+/** 
+ * Action:       SHOW
+ * Method:       GET
+ * URI:          /api/articles/542452
+ * Description:  Get An Article by Article ID
+ */
+
+
+/** 
+ * Action:       CREATE
+ * Method:       POST
+ * URI:          /api/articles
+ * Description:  Create a New Article
+ */
+
+ /** 
+ * Action:       UPDATE
+ * Method:       PATCH           // PATCH for multiple fields, PUT for one field
+ * URI:          /api/articles/54525424
+ * Description:  Update An Article by Article ID
+ */
+
+ /** 
+ * Action:       DESTROY
+ * Method:       DELETE           
+ * URI:          /api/articles/54254252
+ * Description:  Delete An Article by Article ID
+ */
+
+
+// Export the Router so we can use it in the server.js file
+module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,35 @@
-// Require necssary NPM Packages
+// Require Necessary NPM Packages
 const express = require('express');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-// Instantiate Express Application Object
+// Require Route Files
+const indexRouter = require('./app/routes/index');
+const articleRouter = require('./app/routes/articles');
+
+// Require DB Configuration File
+const db = require('./config/db');
+
+// Establish Database Connection
+mongoose.connect(db, {useNewUrlParser: true});
+
+// to make sure its Connected to Mongo
+mongoose.connection.once('open', () => {
+    console.log('Connected to Mongo');
+})
+
+
+// Instantiate Express Application Object || Make an Express App
 const app = express();
 
-// Define PORT for the API to run on
-const port = process.env.PORT || 5000; // this mean its will take the enviroment port, if its not available will take 5000 port
+// Define a PORT for the API to run on 
+const port = process.env.PORT || 5000; // if the env has a number use it if not use 5000 || proce... comes from node
 
-// start the server to listen for requests on a given port
-app.listen(port, () => {
+    /*** ROUTES ***/
+// Mount imported Routers
+app.use(indexRouter);
+app.use(articleRouter);
+
+// Start the server to listen for requests on a given port
+app.listen(port, () =>{
     console.log(`blogy is listening on port ${port}`);
 });
